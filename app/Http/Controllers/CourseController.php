5f6b9e6d1bde63eff1e64690d64292b38f97a836
xanushkasrivastavax <anushkasrivastavax@gmail.com>
2022-04-29 11:48:19 +0530

    change

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -25,47 +25,9 @@ class CourseController extends Controller
             return back()->withError($exception->getMessage());
         }
     }
-    public function editcourse($id)
-    {
-        try {
-            $course = Course::editCourse($id);
-        } catch (\Exception $exception) {
-            return back()->withError($exception->getMessage());
-        }
-        return view('admin.editCourse', compact('course'));
-    }
-    public function postedit($id, Request $request)
-    {
-        $this->validate($request, [
-
-            'cname' => 'required|string|max:255',
-
-            'duration' => 'required|int|max:11',
-
-        ]);
-        try {
-            $course = Course::updateCourse($id);
-
-            $course->cname = $request->input('cname');
-            $course->duration = $request->input('duration');
-
-            $course->save();
-        } catch (\Exception $exception) {
-            return back()->withError($exception->getMessage());
-        }
-        return Redirect('/cinfo');
-    }
     public function getStudent($id, Request $request)
     {
         $course = Course::getStudent($id);
     }
-    public function uniquecourse()
-    {
-        try {
-            $course = Course::get()->distinct();
-        } catch (\Exception $exception) {
-            return back()->withError($exception->getMessage());
-        }
-        return view('admin.vCourse', compact('course'));
-    }
+ 
 }
